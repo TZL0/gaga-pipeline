6e7ac77921153af7b6596a4bc7d205a00a9d5384
Qi Ting Ng <ngqiting@gmail.com>
2025-04-12 00:09:39 +0800

    Fix Slider not having fill

--- cp4101-client/src/components/common/Slider/Slider.js
+++ cp4101-client/src/components/common/Slider/Slider.js
@@ -1,18 +1,27 @@
+import { useEffect, useRef } from 'react';
 import styles from './Slider.module.css';
 
 const Slider = ({
     label,
     displayedValue,
     style,
+    value,
     ...rest
 }) => {
+    const sliderRef = useRef(null);
+
+    useEffect(() => {
+        const val = (sliderRef.current.value - sliderRef.current.min) / (sliderRef.current.max - sliderRef.current.min) * 100;
+        sliderRef.current.style.background = `linear-gradient(to right, var(--color-primary) ${val}%, whitesmoke ${val}%)`;
+    }, [value])
+
     return (
         <div className={styles.container} style={style}>
             <div className={styles.labelRow}>
                 {label && <div className={styles.label}>{label}</div>}
                 {displayedValue && <div className={styles.value}>{displayedValue}</div>}
             </div>
-            <input className={styles.slider} type="range" {...rest}/>
+            <input className={styles.slider} type="range" ref={sliderRef} value={value} {...rest}/>
         </div>
     );
 };
