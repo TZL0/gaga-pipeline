6691b99c788280f364cc6a0d9d75dee9665a6570
Qi Ting Ng <ngqiting@gmail.com>
2025-04-11 20:07:05 +0800

    Create RadioButtonGroup common component

---
+++ cp4101-client/src/components/common/RadioButtonGroup/RadioButtonGroup.js
@@ -0,0 +1,43 @@
+import styles from './RadioButtonGroup.module.css';
+
+const RadioButtonGroup = ({
+    label,
+    value,
+    setValue,
+    options = [{ 'label': '', 'value': ''}],
+    style,
+    disabled,
+}) => {
+    return (
+        <div className={styles.container}  style={style}>
+            {label && <div className={styles.label}>{label}</div>}
+            {options.map((opt, index) => {
+                return (
+                    <div
+                        className={styles.option}
+                        onClick={disabled ? undefined : () => setValue(opt.value)}
+                        style={{
+                            border: value === opt.value
+                                ? '1px solid var(--color-primary)'
+                                : '1px solid var(--color-border',
+                            opacity: disabled ? 0.5 : 1,
+                            cursor: disabled ? 'not-allowed' : 'pointer',
+                        }}
+                    >
+                        <input
+                            type="radio"
+                            value={opt.value}
+                            className={styles.radiobutton}
+                            checked={value === opt.value}
+                            onChange={(e) => setValue(e.target.value)}
+                            disabled={disabled}
+                        />
+                        {opt.label}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default RadioButtonGroup;
